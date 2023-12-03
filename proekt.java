5330ebda2e0237ce9dff2dbc6879395bd60ad4fe
Sara Usevska <sarausevska42@yahoo.com>
2023-12-03 15:43:29 +0100

    first commit

---
+++ proekt.java
@@ -0,0 +1,26 @@
+package project_15;
+import java.util.Scanner;
+public class proekt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int x=1, y=1, z=1;
+    if(x>y || z>y) {
+	  
+    System.out.println("1");
+    } else {
+    System.out.println("2");
+    }
+	if (Math.abs(x - y)	>=z) {
+		if(x > y) {
+			
+			System.out.println("3");
+		
+		}else 
+				System.out.println("4");
+		
+	    }else {
+			System.out.println("5");
+	 }
+   }
+ }
